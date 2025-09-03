40dc4e654b3708e02690b1ae416867dc3368b64b
robindw93 <119764512+robindw93@users.noreply.github.com>
2025-09-03 02:07:05 +0200

    Initieel projectbestand

---
+++ app/src/main/java/com/example/iptvtvapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.iptvtvapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(color = MaterialTheme.colorScheme.background) {
+                Text("Welkom bij IPTVTVApp op Android TV!")
+            }
+        }
+    }
+}
